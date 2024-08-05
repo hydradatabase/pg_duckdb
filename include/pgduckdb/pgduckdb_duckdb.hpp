9c0ecba0f6c092872b699759eb53237d5e3fd1f8
Jelte Fennema-Nio <github-tech@jeltef.nl>
2024-08-05 15:56:45 +0200

    Support schemas

--- include/pgduckdb/pgduckdb_duckdb.hpp
+++ include/pgduckdb/pgduckdb_duckdb.hpp
@@ -13,5 +13,6 @@ namespace pgduckdb {
 extern duckdb::unique_ptr<duckdb::DuckDB> DuckdbOpenDatabase();
 extern duckdb::unique_ptr<duckdb::Connection> DuckdbCreateConnection(List *rtables, PlannerInfo *plannerInfo,
                                                                      List *neededColumns, const char *query);
+duckdb::unique_ptr<duckdb::QueryResult> RunQuery(duckdb::Connection const &connection, const std::string &query);
 
 } // namespace pgduckdb
